b68c44d383807c55597f4ede19b014b094e3395a
SimeonErmolin <ermolinsemen96@gmail.com>
2022-02-23 20:55:23 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -8,19 +8,29 @@ modal();
 
 requestToServer(URL.REQUEST_MESSAGES, 'GET').then(response => renderServerMessages(response.messages));
 
-function renderServerMessages(messages) {
+async function renderServerMessages(messages) {
+  const userData = await requestToServer(URL.REQUEST_ME, 'GET');
+
   messages.forEach(item => {
     const template = UI.CHAT.TEMPLATE.content.cloneNode(true);
 
-    template.querySelector('.nickname').innerHTML = item.username;
-    template.querySelector('.message').innerHTML = item.message;
+    if (userData.email == item.user.email) {
+      template.querySelector('.nickname').innerHTML = 'Я';
+      template.querySelector('.user-message').classList.add('my-message');
+    } else {
+      template.querySelector('.nickname').innerHTML = item.user.name;
+    }
+
+    template.querySelector('.message').innerHTML = item.text;
     template.querySelector('.time').innerHTML = formatTime(item.createdAt);
 
     UI.CHAT.WINDOW.append(template);
   })
+
+  UI.CHAT.WINDOW.scrollTo(0, UI.CHAT.WINDOW.scrollHeight);
 }
 
-const socket = new WebSocket(`ws://chat1-341409.oa.r.appspot.com/websockets?${getCookie('token')}`);
+const socket = new WebSocket(`wss://chat1-341409.oa.r.appspot.com/websockets?${getCookie('token')}`);
 
 socket.onmessage = function (event) {
   renderWebSocketMessage(JSON.parse(event.data));
